292bbc155fe3a8fa10cca90360258f40813e825c
Nikita Volodin <volodin.n@gmail.com>
2013-06-13 11:00:35 -0300

    Create mining_turtle_awaitCommand.lua
    Program that waits command to dig from mining_computer, used by mining turtles

---
+++ mining_turtle_awaitCommand.lua
@@ -0,0 +1,18 @@
+local modemSide = "right"
+if not rednet.isOpen(modemSide) then
+  rednet.open(modemSide)
+end
+print("Port opened")
+print("Waiting for incoming messages..")
+
+while true do
+  senderID, message, distance = rednet.receive()
+  io.write(message)
+  if shell.run(message.." .. ") then
+    print("done")
+  else
+    print("error")
+  end
+  
+  rednet.send(senderID, "finished")
+end
