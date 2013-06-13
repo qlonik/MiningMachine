307d991f674e438062492a229b6557bfbe66c723
Nikita Volodin <volodin.n@gmail.com>
2013-06-13 12:21:03 -0300

    Update mining_computer_cycle.lua
    Added variable amount of steps, used by mining computer

--- mining_computer_cycle.lua
+++ mining_computer_cycle.lua
@@ -1,19 +1,42 @@
 local direction = "forward"
 local modemSide = "right"
+local tArgs = {...}
+local steps = 1
+
+function printUsage()
+  print("Usage: "..fs.getName(shell.getRunningProgram()).." [steps]")
+  return
+end
+
+if #tArgs > 1 then
+  printUsage()
+else 
+  steps = tonumber(tArgs[1])
+end
  
 if not rednet.isOpen(modemSide) then
   rednet.open(modemSide)
 end
- 
-shell.run("mvFrame", direction)
- 
--- send dig command to all turtles
-for i = 3, 18 do
-  rednet.send(i, "cycle")
-end
- 
-local i = 0
-while i ~= 16 do
-  senderID, message, distance = rednet.receive()
-  i = i + 1
+
+print("Running")
+
+for i = 1, steps do
+  if steps > 1 then
+    io.write("Step #"..i.." out of "..steps)
+  end
+  
+  shell.run("mvFrame", direction)
+
+  -- send dig command to all turtles
+  for i = 3, 18 do
+    rednet.send(i, "cycle")
+  end
+
+  local i = 0
+  while i ~= 16 do
+    senderID, message, distance = rednet.receive()
+    i = i + 1
+  end
+  
+  print(" .. done")
 end
