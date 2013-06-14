f4e5f7cffef718f7a57d8e7ab27f3f7f3c84d2d0
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-06-14 00:45:36 -0300

    Updated formatting; Created variable for Mining Well spot

--- mining_turtle_cycle.lua
+++ mining_turtle_cycle.lua
@@ -1,32 +1,35 @@
 local timeout = 45
- 
+local miningWellSpot = 1
+
 function deploy()
-  turtle.select(1)
-  turtle.place()
+	turtle.select(miningWellSpot)
+	turtle.place()
 end
- 
+
 function clearInv()
-  local time = 0
-  while (turtle.getItemCount(1) == 0) and (time < timeout) do
-    sleep(1)
-    time = time + 1
-  end
-  sleep(2)
-  while turtle.getItemCount(1) > 0 do
-    for i = 1, 16 do
-      turtle.select(i)
-      turtle.dropDown()
-    end
-    sleep(1)
-  end
-  turtle.select(1)
+	local time = 0
+	while (turtle.getItemCount(1) == 0) and (time < timeout) do
+		sleep(1)
+		time = time + 1
+	end
+	sleep(2)
+
+	while turtle.getItemCount(1) > 0 do
+		for i = 1, 16 do
+			turtle.select(i)
+			turtle.dropDown()
+		end
+		sleep(1)
+	end
+
+	turtle.select(miningWellSpot)
 end
- 
+
 function getMiner()
-  turtle.select(1)
-  turtle.dig()
+	turtle.select(miningWellSpot)
+	turtle.dig()
 end
- 
+
 deploy()
 clearInv()
 getMiner()
