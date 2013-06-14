91ec41595d1e2f06b99d29a9e42e87cfb88381b9
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-06-14 00:47:44 -0300

    Updated formatting; Added comment for program

--- mining_turtle_restore.lua
+++ mining_turtle_restore.lua
@@ -1,6 +1,10 @@
+-- Run this if finished cycle program unexpectedly
+-- Be careful about sending Mining Well into 
+-- inventory below
+
 for i = 1, 16 do
-  turtle.select(i)
-  turtle.dropDown()
+	turtle.select(i)
+	turtle.dropDown()
 end
 
 turtle.select(1)
