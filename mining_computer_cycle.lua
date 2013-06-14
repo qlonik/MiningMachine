e345618dd36725d1904e78e5772bdeb58a3a540e
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-06-14 00:23:10 -0300

    Updated formatting, used by mining computer

--- mining_computer_cycle.lua
+++ mining_computer_cycle.lua
@@ -1,45 +1,50 @@
 local direction = "forward"
 local modemSide = "right"
-local tArgs = {...}
 local steps = 1
+
 local turtles = 16
 local firstTurtle = 3
+local tArgs = {...}
 
 function printUsage()
-  print("Usage: "..fs.getName(shell.getRunningProgram()).." [steps]")
-  return
+	print("Usage: "..fs.getName(shell.getRunningProgram()).." [steps]")
+	return
 end
 
 if #tArgs > 1 then
-  printUsage()
+	printUsage()
 else
-  local tmp_steps = tonumber(tArgs[1])
-  if (tArgs[1] ~= nil) and (tmp_steps > 1) then
-    steps = tmp_steps
-  end
+	local tmp_steps = tonumber(tArgs[1])
+
+	if (tmp_steps ~= nil) and (tmp_steps > 1) then
+		steps = tmp_steps
+	else
+		printUsage()
+	end
 end
- 
+
 if not rednet.isOpen(modemSide) then
-  rednet.open(modemSide)
+	rednet.open(modemSide)
 end
 
 for i = 1, steps do
-  if steps > 1 then
-    io.write("Step #"..i.." out of "..steps)
-  end
+	if steps > 1 then
+		io.write("Step #"..i.." out of "..steps)
+	end
 
-  -- send dig command to all turtles
-  for j = firstTurtle, (firstTurtle + turtles - 1) do
-    rednet.send(j, "cycle")
-  end
+	-- send dig command to all turtles
+	for j = firstTurtle, (firstTurtle + turtles - 1) do
+		rednet.send(j, "cycle")
+	end
 
-  local j = 0
-  while j < turtles do
-    senderID, message, distance = rednet.receive()
-    j = j + 1
-  end
-  
-  shell.run("mvFrame", direction)
-  
-  print(" .. done")
+	-- wait for answer from all turtles
+	local j = 0
+	while j < turtles do
+		senderID, message, distance = rednet.receive()
+		j = j + 1
+	end
+
+	shell.run("mvFrame", direction)
+
+	print(" .. done")
 end
