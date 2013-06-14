a2b2b2873c3e4681ca2794d4d72a6243acafbfb9
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-06-14 01:02:42 -0300

    Code to run on computer to install all scripts

---
+++ mining_computer_download.lua
@@ -0,0 +1,6 @@
+shell.run("pastebin", "get", "g49Fbtc6", "github")
+
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_computer_startup.lua", "startup")
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_computer_autocycle.lua", "autocycle")
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_computer_cycle.lua", "cycle")
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_computer_mvFrame.lua", "mvFrame")
