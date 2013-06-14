be51080b6cceaaab8acc2f6d9165817b5450155a
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-06-14 00:58:59 -0300

    Code to run on turtle to install all scripts

---
+++ mining_turtle_download.lua
@@ -0,0 +1,6 @@
+shell.run("pastebin", "get", "g49Fbtc6", "github")
+
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_turtle_startup.lua", "startup")
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_turtle_cycle.lua", "cycle")
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_turtle_awaitCommand.lua", "awaitCommand")
+shell.run("github", "get", "https://raw.github.com/qlonik/MiningMachine/master/mining_turtle_restore.lua", "restore")
